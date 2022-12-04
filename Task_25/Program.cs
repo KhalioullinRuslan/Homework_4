3c85c7712ec0468419b3d6f2dd4099965ce39322
unknown <xarus82@mail.ru>
2022-12-04 23:18:26 +0300

    Задача о возведении в степень

---
+++ Task_25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int NumbersAB(string degree)
+{
+    Console.Write(degree);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int numberA = NumbersAB("Введите число А: ");
+int numberB = NumbersAB("Введите число В: ");
+
+double exp = Math.Pow(numberA, numberB);
+Console.WriteLine($"Степень числа {numberA} в {numberB} = {Math.Round(exp, 2)}");
